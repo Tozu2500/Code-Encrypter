3759b2acf331206f554723d0291618ff250941c5
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-12-17 18:48:11 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-purple-600 to-blue-600">
+      <h1 className="text-5xl font-bold text-white">Hello World</h1>
+    </div>
+  )
+}
