8581922c1e7f4fcc344466fa2b56d508ad352164
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2025-12-17 20:08:43 +0200

    Delete src directory

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-purple-600 to-blue-600">
-      <h1 className="text-5xl font-bold text-white">Hello World</h1>
-    </div>
-  )
-}
