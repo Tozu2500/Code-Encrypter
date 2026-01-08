dab279ea08656206167aa163cc6be0d0acb4b0a1
Tomi Louhiniitty <tomi.louhiniitty@gmail.com>
2026-01-08 19:51:50 +0200

    Add files via upload

---
+++ src/components/common/Toast.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import styled from 'styled-components';
+import { motion, AnimatePresence } from 'framer-motion';
+import { ToastMessage as ToastMessageType } from '@customTypes/app.types';
+import { FiCheckCircle, FiXCircle, FiAlertCircle, FiInfo } from 'react-icons/fi';
